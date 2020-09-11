aa8b83fb3d5305b349a9cb056da7dbc8e2bd4af9
CaryTanner <cary.landon@gmail.com>
2020-09-11 14:32:51 +0200

    first commit

---
+++ scripts.js
@@ -0,0 +1,7 @@
+let navBar = document.getElementById("nav-bar");
+let burgerButton = document.getElementById("burger");
+
+burgerButton.addEventListener("click", function(){
+  navBar.classList.toggle("show-nav");
+});
+
